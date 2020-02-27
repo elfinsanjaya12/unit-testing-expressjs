360a4f937e5e07aa6b2929d5936b37c219e75250
fari <fari.msenju@gmail.com>
2020-02-27 14:30:59 +0700

    refactor: add methods and process beforeCreate user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,8 @@
 'use strict';
+
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
@@ -8,6 +12,27 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   User.associate = function (models) {
     // associations can be defined here
-  };
+  };  
+
+  User.beforeCreate((user, _) => {
+    const salt = bcrypt.genSaltSync(10);
+    user.password = bcrypt.hashSync(user.password, salt);
+  });
+
+  User.prototype.generateAuthToken =  async (user) => {
+    const token = jwt.sign({
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name
+      }
+    }, 'secret');    
+    return token;
+  }
+
+  User.prototype.checkPassword = (password, userPassword) => {
+    return bcrypt.compareSync(password, userPassword);
+  }
+
   return User;
 };
