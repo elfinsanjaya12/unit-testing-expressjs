04855b9cf13a0970bacc1326d61114090df1ddb8
fari <fari.msenju@gmail.com>
2020-02-27 14:29:21 +0700

    refactor: add root endpoint and export app

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ const userRouter = require("./routers/users");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    message: "Welcome to Backend API"
+  });
+});
+
 app.use("/api/v1/users", userRouter);
 
 const port = 3000;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Server Running on port ${port}!`));
+
+module.exports = app;
